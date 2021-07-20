3ed19c6aafc22bc0cc4049f97d1d3cae60854d68
Thai An <kieuthaian.1999@gmail.com>
2021-07-20 16:42:56 +0700

    index

---
+++ src/pp.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+
+
+function pp() {
+    return (
+        <div className="pp">
+            <header className="pp-header">
+                <img src={logo} className="pp-logo" alt="logo" />
+                <p>
+                    Học lập trình React
+                </p>
+
+            </header>
+        </div>
+    );
+}
+
+export default pp;
